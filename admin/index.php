29c998bb36121547964e37ed08b360ccac2297bf
innovasoftware1 <gerencia@innovapublicidad.com.co>
2024-12-05 16:37:09 -0500

    prueba00.1

--- admin/index.php
+++ admin/index.php
@@ -83,7 +83,7 @@ $totaCiudades = obtenerTotalRegistros('ciudades');
         <div class="contenedor-principal">
             <div id="dashboard">
                 <h2>Dashboard Principal</h2>
-                <h2>menuu</h2>
+                <h2>menuu prueba</h2>
                 <br>
                 <hr>
                 <div class="contenedor-cajas-info">
